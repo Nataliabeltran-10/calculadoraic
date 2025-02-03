989778e18e8346bbf39da781ca699c458d598879
Nataliabeltran-10 <nataliabeltranfranco964@gmail.com>
2025-02-03 11:18:26 +0100

    Nuevas cuentas

--- calculadora.js
+++ calculadora.js
@@ -7,6 +7,17 @@ function sumar(a, b) {
   function restar(a, b) {
     return a - b;
   }
+
+  function multiplicar(a, b){
+    return a * b; 
+  }
+
+  function dividir(a, b){
+    return a/b;
+  }
   
+  function raizCuadrada(a){
+    return Math.sqrt(a);
+  }
   
-  module.exports = { sumar, restar };
+  module.exports = { sumar, restar, multiplicar, dividir, raizCuadrada };
